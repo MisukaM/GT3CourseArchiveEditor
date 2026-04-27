42edbbf2c3a3c2d299236e0f53c1e24edd2894e7
Misuka ミスカ <122004189+MisukaM@users.noreply.github.com>
2026-04-27 03:17:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #GT3CourseArchiveEditor
 import os
 import struct
-import time
+import argparse
 
 print("GT3CourseArchiveEditor - Made by Misuka")
 
@@ -12,8 +12,8 @@ archive_filename_table = {
 1: "01_MainCourseModel.mdl", 2: "02_MainCourseVisionList.lv", 3: "03_LODCourseVisionList.lv", 4: "04_CarReflectionVisionList.lv", 5: "05_CarReflectionModel.mdl", 6: "06_CourseReflectionVisionList.lv", 7: "07_ExtraCourseVisionList.lv",
 9: "08_RoadSurfaceModel.mdl", 10: "09_RoadSurfaceVisionList.lv", 13: "10_RoadSurfaceModel2.mdl", 14: "11_RoadSurfaceVisionList2.lv", 21: "12_SkyboxModel.mdl", 22: "13_CourseBackgroundModel.mdl", 23: "14_CarReflectionModel2.mdl", 
 24: "15_LODSkyboxModel.mdl", 25: "16_UnknownNightCourseFlag.bin", 26: "17_SmokeTexture.img", 27: "18_ExtraCourseModel.mdl", 28: "19_CourseRunwayData.rwy", 29: "20_CourseLightFlareData.gtfx", 30: "21_BillboardData.blbd", 31: "22_DrivingLines.ad", 32: "23_EnvironmentParameters.envptr",
-33: "24_CourseMiniMapData.map", 34: "25_CourseReplayCameras.cam", 35: "26_CourseSoundData.crss", 36: "27_UnknownCourseModel.mdl", 37: "28_UnknownCourseFile3", 38: "29_UnknownCourseFile4", 39: "30_UnknownCourseFile5",
-40: "31_UnknownCourseFile6", 41: "32_CameraFile1.cam", 42: "33_CameraFile2.cam", 43: "34_UnknownCourseModel2.mdl"
+33: "24_CourseMiniMapData.map", 34: "25_CourseReplayCameras.cam", 35: "26_CourseSoundData.crss", 36: "27_GadgetModels.mdl", 37: "28_AIParamRWD.bin", 38: "29_AIParamFWD.bin", 39: "30_AIParam4WD.bin",
+40: "31_AIParamGTR.bin", 41: "32_CameraFile1.cam", 42: "33_CameraFile2.cam", 43: "34_UnknownCourseModel2.mdl"
 }
 
 #program functions
@@ -118,11 +118,11 @@ def crs_repack(course):
         print("Files found:", len(course_files))
         print()
 
-        with open(course+"_new", "wb") as f:
+        with open(course, "wb") as f:
             f.write(b'\x00' * course_archive_header_size) #write header bytes
             offset = course_archive_header_size
 
-        with open(course+"_new", "rb+") as f:
+        with open(course, "rb+") as f:
             for key, value in archive_filename_table.items():
                 if value not in course_files:
                     print(f"Skipping missing file: {value}")
@@ -137,9 +137,10 @@ def crs_repack(course):
                     f.seek(offset)
                     f.write(read_data)
                     offset += current_file_size
-        print("This is normal, not every course archive uses every file slot.")
         print()
-        print(f'Course repacked successfully into "{course}_new"')
+        print("If files were skipped, this is normal, as not every course uses every file slot.")
+        print()
+        print(f'Course repacked successfully into "{course}"')
         print()
     
     except FileNotFoundError as e:
@@ -149,44 +150,48 @@ def crs_repack(course):
     except Exception as e:
         print(f"An unexpected error occurred during repacking: {e}")
 
-#user action
-def main():
-    current_dir = os.getcwd()
-    file_count_in_dir = count_files_in_dir(current_dir)
-    while True:
-        print()
-        user_command = input('Choose action: "B"=Build, "U"=Unpack, "BA"=BuildAll, "UA"=UnpackAll, "E"=Exit: ')
-        print("File Count in directory;", file_count_in_dir)
-        if user_command.lower() == "e":
-            print("Exiting...")
-            time.sleep(1.5)
-            break
-        elif user_command.lower() == "b":
-            selected_course = input("Input course filename: ")
-            print()
-            crs_repack(selected_course)
-        elif user_command.lower() == "u":
-            selected_course = input("Input course filename: ")
-            file_count = count_files(selected_course)
-            print("Files found:", file_count)
-            print()
-            crs_unpack(selected_course)
-        elif user_command.lower() == "ba":
-            for file in os.listdir(current_dir):
-                if os.path.isfile(file):
-                    selected_course = os.fsdecode(file)
-                    print()
-                    crs_repack(selected_course)
-        elif user_command.lower() == "ua":
-            for file in os.listdir(current_dir):
-                if os.path.isfile(file):
-                    selected_course = os.fsdecode(file)
-                    file_count = count_files(selected_course)
-                    print("Files found:", file_count)
-                    print()
-                    crs_unpack(selected_course)
-        else:
-            print("Invalid command.")
 
 if __name__ == "__main__":
-    main()
+    current_dir = os.getcwd()
+    file_count_in_dir = count_files_in_dir(current_dir)
+
+    parser = argparse.ArgumentParser(description="GT3 Course Archive Unpacker/Rebuilder")
+    parser.add_argument("--build", metavar="FILE", help="Rebuild course archive")
+    parser.add_argument("--unpack", metavar="FILE", help="Unpacks course archive")
+    parser.add_argument("--buildall", action="store_true", help="Builds all course files in the directory")
+    parser.add_argument("--unpackall", action="store_true", help="Unpacks all course files in the directory")
+    parser.add_argument("--exit", action="store_true", help="Exits the program")
+    args = parser.parse_args()
+
+    if args.build:
+        crs_repack(args.build.removesuffix("_out"))
+
+    elif args.unpack:
+        file_count = count_files(args.unpack)
+        print("Files found:", file_count)
+        print()
+        crs_unpack(args.unpack)
+        
+    elif args.buildall:
+        for file in os.listdir(current_dir):
+            if os.path.isdir(file) and file.endswith("_out"):
+                selected_course = os.fsdecode(file)[:-4]
+                print()
+                crs_repack(selected_course)
+
+    elif args.unpackall:
+        for file in os.listdir(current_dir):
+            if os.path.isfile(file):
+                selected_course = os.fsdecode(file)
+                file_count = count_files(selected_course)
+                print("Files found:", file_count)
+                print()
+                crs_unpack(selected_course)
+
+    elif args.exit:
+        exit()
+
+    else:
+        parser.print_help()
+
+    #main()
